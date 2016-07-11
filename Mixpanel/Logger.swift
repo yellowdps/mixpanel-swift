a3cc2b43c65680fae5f27004c2ae29d2822c597f
samgreen <greensamuelm@gmail.com>
2016-07-11 10:58:48 -0700

    Combine logging files, fix comment in Logger.swift

--- Mixpanel/Logger.swift
+++ Mixpanel/Logger.swift
@@ -1,6 +1,6 @@
 //
-//  MPLogger.swift
-//  MPLogger
+//  Logger.swift
+//  Logger
 //
 //  Created by Sam Green on 7/8/16.
 //  Copyright © 2016 Mixpanel. All rights reserved.
@@ -10,8 +10,58 @@ import Foundation
 
 let Log = Logger(logging: PrintLogging())
 
+/// This defines the various levels of logging that a message may be tagged with. This allows hiding and
+/// showing different logging levels at run time depending on the environment
+enum LogLevel: String {
+    /// Logging displays nothing
+    case None
+    
+    /// Logging displays *all* logs and additional debug information that may be useful to a developer
+    case Debug
+    
+    /// Logging displays *all* logs (**except** debug)
+    case Info
+    
+    /// Logging displays *only* warnings and above
+    case Warning
+    
+    /// Logging displays *only* errors and above
+    case Error
+}
+
+/// This holds all the data for each log message, since the formatting is up to each
+/// logging object. It is a simple bag of data
+struct LogMessage {
+    /// The file where this log message was created
+    let file: String
+    
+    /// The function where this log message was created
+    let function: String
+    
+    /// The text of the log message
+    let text: String
+    
+    /// The level of the log message
+    let level: LogLevel
+    
+    init(path: String, function: String, text: String, level: LogLevel) {
+        if let file = path.components(separatedBy: "/").last {
+            self.file = file
+        } else {
+            self.file = path
+        }
+        self.function = function
+        self.text = text
+        self.level = level
+    }
+}
+
+/// Any object that conforms to this protocol may log messages
+protocol Logging {
+    func addMessage(message: LogMessage)
+}
+
 class Logger {
-    // MARK: Private
     private var loggers = [Logging]()
     private var enabledLevels = Set<LogLevel>()
     
